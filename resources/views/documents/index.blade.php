879aa565787c50b5f51dd62c3b8a01837fe4134f
ShengChih <ms0529756@gmail.com>
2019-07-06 07:39:31 +0800

    Add Laravel default auth

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -4,6 +4,7 @@
 <div class="row">
 <div class="col-sm-12">
     <h2 class="display-6">文章列表</h1>
+    <a href="{{ route('documents.create') }}" class="new-btn btn btn-primary">新增</a>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -24,7 +25,7 @@
                 <td>{{$doc->ctime}}</td>
                 <td>{{$doc->mtime}}</td>
                 <td>
-                    <a href="{{ route('documents.edit',$doc->id) }}" class="btn btn-primary">編輯</a>
+                    <a href="{{ route('documents.edit', $doc->id) }}" class="btn btn-primary">編輯</a>
                 </td>
                 <td>
                     <form action="{{ route('documents.destroy', $doc->id)}}" method="post">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::resource('documents', 'DocumentController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
